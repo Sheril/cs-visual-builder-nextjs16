94c28859febe9742896a117d83952bcf9d411b79
sjs <sheril.subramanian@contentstack.com>
2025-10-28 23:38:01 -0400

    refinement

--- components/LivePreview/index.tsx
+++ components/LivePreview/index.tsx
@@ -3,11 +3,6 @@
 import ContentstackLivePreview from "@contentstack/live-preview-utils";
 import { useEffect } from "react";
 
-const CS_API_KEY = process.env.NEXT_PUBLIC_CS_API_KEY ?? "";
-const CS_ENVIRONMENT = process.env.NEXT_PUBLIC_CS_ENVIRONMENT ?? "";
-const CS_HOST = process.env.NEXT_PUBLIC_CS_HOST ?? "";
-
-
 export default function LivePreviewInitComponent() {
   useEffect(() => {
     // Initialize the Contentstack Live Preview SDK
@@ -18,15 +13,15 @@ export default function LivePreviewInitComponent() {
       ssr: true,
       editButton: { enable: true },
       stackDetails: {
-        apiKey: CS_API_KEY,
-        environment: CS_ENVIRONMENT,
+        apiKey: process.env.NEXT_PUBLIC_CS_API_KEY ?? "",
+        environment: process.env.NEXT_PUBLIC_CS_ENVIRONMENT ?? "",
         branch: "main",
       },
       // Enable Visual Builder
       mode: "builder",
       clientUrlParams: {
         protocol: "https",
-        host: CS_HOST,
+        host: process.env.NEXT_PUBLIC_CS_HOST ?? "",
         port: 443,
       },
     });
